b86a9bed965c18f0cdeb895cb27063eafe2bff52
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-04 12:41:50 -0400

    set up for deployment migration but am hitting an error

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  JWT_SECTRET: process.env.JWT_SECTRET || 'change-this-secret',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/high-low',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/high-low-test',
-  JWT_SECTRET: process.env.JWT_SECTRET || 'change-this-secret',
 };
