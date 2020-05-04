42e2a06ecf33b79f52a266159863cd094fa666c5
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-04 11:26:22 -0400

    renamed variables for heroku db

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/high-low',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/high-low',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/high-low-test',
   JWT_SECTRET: process.env.JWT_SECTRET || 'change-this-secret',
 };
