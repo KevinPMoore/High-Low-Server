88bbf49b7e04f334bf93138a13553220d61efd15
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-04 11:01:01 -0400

    set up for test deployment

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const usersRouter = require('./users/users-router');
@@ -16,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(helmet());
 
 app.use('/api/users', usersRouter);
