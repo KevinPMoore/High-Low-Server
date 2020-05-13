50290fb0478dea4d337d28b0c46718cbf2708f59
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-13 10:33:11 -0400

    added landing.test.js and corrected landing page text

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/high-low',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/high-low-test',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://high-low.now.sh/',
 };
